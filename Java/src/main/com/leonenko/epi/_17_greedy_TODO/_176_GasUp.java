9c7093fc8098d247d3820f9c147d663aa3304d3d
Oleksandr Leonenko <aleks.leonenko@gmail.com>
2019-01-08 03:52:51 -0800

    Implement: gas up

---
+++ Java/src/main/com/leonenko/epi/_17_greedy_TODO/_176_GasUp.java
@@ -0,0 +1,50 @@
+package com.leonenko.epi._17_greedy_TODO;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Alex on 5/8/2017.
+ */
+public class _176_GasUp {
+
+    private static final int MPG = 20;
+
+    static char findStartCity1(int[] distanceToNext, int[] availableGas, char[] cities) {
+        var visitedCities = new LinkedList<Integer>();
+        var remainingGas = 0;
+        var city = 0;
+
+        while (visitedCities.size() < cities.length) {
+           while (remainingGas < 0 && !visitedCities.isEmpty()) {
+               int visitedCity = visitedCities.poll();
+               remainingGas += -availableGas[visitedCity] + distanceToNext[visitedCity] / MPG;
+           }
+
+           remainingGas += availableGas[city] - distanceToNext[city] / MPG;
+           visitedCities.add(city);
+           city = (city + 1) % cities.length;
+        }
+        System.out.printf("Start at city '%s'", cities[visitedCities.peek()]);
+
+        return cities[visitedCities.peek()];
+    }
+
+    static char findStartCity2(int[] distanceToNext, int[] availableGas, char[] cities) {
+        int minRemainingGas = 0;
+        int minRemainingGasCity = 0;
+        int remainingGas = 0;
+
+        for (int currCity = 1; currCity < cities.length; currCity++) {
+            int prevCity = currCity - 1;
+            remainingGas += availableGas[prevCity] - distanceToNext[prevCity] / MPG;
+
+            if (remainingGas < minRemainingGas) {
+                minRemainingGasCity = currCity;
+                minRemainingGas = remainingGas;
+            }
+        }
+        System.out.printf("Start at city '%s'", cities[minRemainingGasCity]);
+
+        return cities[minRemainingGasCity];
+    }
+}
---
+++ Java/src/tests/com/leonenko/epi/_17_greedy_TODO/_176_GasUpTest.java
@@ -0,0 +1,24 @@
+package com.leonenko.epi._17_greedy_TODO;
+
+import org.junit.jupiter.api.Test;
+
+import static com.leonenko.epi._17_greedy_TODO._176_GasUp.findStartCity1;
+import static com.leonenko.epi._17_greedy_TODO._176_GasUp.findStartCity2;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _176_GasUpTest {
+
+    private static final int[] milesToNext = new int[]{900, 600, 200, 400, 600, 200, 100};
+    private static final int[] gasVol = new int[]{50, 20, 5, 30, 25, 10, 10};
+    private static final char[] cities = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+
+    @Test
+    void testFindStartCity1() {
+        assertThat(findStartCity1(milesToNext, gasVol, cities)).isEqualTo('D');
+    }
+
+    @Test
+    void testFindStartCity2() {
+        assertThat(findStartCity2(milesToNext, gasVol, cities)).isEqualTo('D');
+    }
+}
