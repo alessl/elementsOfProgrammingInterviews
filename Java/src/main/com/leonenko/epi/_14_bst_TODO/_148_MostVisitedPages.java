b5abf3ea784c2231aba0297287671e3c5ed37ff1
Oleksandr Leonenko <aleks.leonenko@gmail.com>
2019-01-30 02:16:30 -0800

    14.8 Most frequent ids

---
+++ Java/src/main/com/leonenko/epi/_14_bst_TODO/_148_MostVisitedPages.java
@@ -0,0 +1,130 @@
+package com.leonenko.epi._14_bst_TODO;
+
+import java.util.*;
+
+public class _148_MostVisitedPages {
+
+    static class ConstantAddAndLinearKGetImpl {
+        private static Map<String, ListNode> idToNode = new HashMap<>();
+        private static ListNode mostFrequentNode;
+        private static ListNode leastFrequentNode;
+
+        private static final class ListNode {
+            private String id;
+            private int visitCount;
+            private ListNode prev;
+            private ListNode next;
+
+            public ListNode(String id, int visitCount) {
+                this.id = id;
+                this.visitCount = visitCount;
+            }
+        }
+
+        static List<String> getMostFrequentIds(int num) { // O(num)
+            var mostFrequentIds = new ArrayList<String>();
+
+            ListNode curr = mostFrequentNode;
+            for (int i = 0; i < num && curr != null; i++) {
+                mostFrequentIds.add(curr.id);
+
+                curr = curr.next;
+            }
+
+            return mostFrequentIds;
+        }
+
+        static int addId(String id) { // O(1)
+            ListNode listNode = idToNode.computeIfAbsent(id, k -> new ListNode(id, 0));
+
+            listNode.visitCount++;
+
+            if (mostFrequentNode == null) {
+                mostFrequentNode = listNode;
+                leastFrequentNode = listNode;
+            } else if (listNode.visitCount == 1) {
+                leastFrequentNode.next = listNode;
+                listNode.prev = leastFrequentNode;
+                leastFrequentNode = listNode;
+            } else if (listNode != mostFrequentNode && listNode.visitCount > listNode.prev.visitCount) {
+                listNode.prev.next = listNode.next;
+                listNode.next = listNode.prev;
+                listNode.prev = listNode.prev.prev;
+
+                if (listNode.prev == null) {
+                    mostFrequentNode = listNode;
+                } else {
+                    listNode.prev.next = listNode;
+                }
+            }
+
+            return listNode.visitCount;
+        }
+    }
+
+    static class OrderStatisticImpl {
+
+    }
+
+    static class BstPredecessorAndHashMapImpl {
+
+    }
+
+    static class TreeSetAndHashMapImpl {
+
+        private static Map<String, TreeEntry> idToEntry = new HashMap<>();
+        private static NavigableSet<TreeEntry> tree = new TreeSet<>();
+
+        static List<String> getMostFrequentIds(int num) {
+            var mostFrequentIds = new ArrayList<String>();
+            var polledEntries = new ArrayList<TreeEntry>();
+            var actualNum = Math.min(num, tree.size());
+
+            for (int i = 0; i < actualNum; i++) {
+                var entry = tree.pollLast(); // log(n)
+
+                polledEntries.add(entry); // o(1)
+                mostFrequentIds.add(entry.id); // o(1)
+            }
+
+            tree.addAll(polledEntries); // num * log(n)
+
+            return mostFrequentIds;
+        }
+
+        static int addId(String id) {
+            var entry = idToEntry.computeIfAbsent(id, k -> new TreeEntry(id, 0));
+
+            if (entry.visitCount > 0) {
+                tree.remove(entry);
+            }
+
+            entry.visitCount++;
+
+            tree.add(entry);
+
+            return entry.visitCount;
+        }
+
+        private static class TreeEntry implements Comparable<TreeEntry> {
+            private String id;
+            private int visitCount;
+
+            public TreeEntry(String id, int visitCount) {
+                this.id = id;
+                this.visitCount = visitCount;
+            }
+
+            @Override
+            public int compareTo(TreeEntry o) {
+                var comparedByVisitCount = Integer.compare(visitCount, o.visitCount);
+
+                if (comparedByVisitCount != 0) {
+                    return comparedByVisitCount;
+                }
+
+                return o.id.compareTo(id);
+            }
+        }
+    }
+}
---
+++ Java/src/tests/com/leonenko/epi/_14_bst_TODO/_148_MostVisitedPagesTest.java
@@ -0,0 +1,68 @@
+package com.leonenko.epi._14_bst_TODO;
+
+import com.leonenko.epi._14_bst_TODO._148_MostVisitedPages.ConstantAddAndLinearKGetImpl;
+import com.leonenko.epi._14_bst_TODO._148_MostVisitedPages.TreeSetAndHashMapImpl;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _148_MostVisitedPagesTest {
+
+    @Test
+    void testMostVisitedIds1() {
+        var num = 3;
+
+        doTest(() -> TreeSetAndHashMapImpl.getMostFrequentIds(num), TreeSetAndHashMapImpl::addId);
+    }
+
+    @Test
+    void testMostVisitedIds2() {
+        var num = 3;
+
+        doTest(() -> ConstantAddAndLinearKGetImpl.getMostFrequentIds(num), ConstantAddAndLinearKGetImpl::addId);
+    }
+
+    private void doTest(Supplier<List<String>> mostFrequentSupplier, Function<String, Integer> addIdConsumer) {
+        assertThat(mostFrequentSupplier.get()).isEmpty();
+
+        assertThat(addIdConsumer.apply("1")).isEqualTo(1);
+        assertThat(mostFrequentSupplier.get()).containsExactly("1");
+
+        assertThat(addIdConsumer.apply("2")).isEqualTo(1);
+        assertThat(mostFrequentSupplier.get()).containsExactly("1", "2");
+
+        assertThat(addIdConsumer.apply("1")).isEqualTo(2);
+        assertThat(mostFrequentSupplier.get()).containsExactly("1", "2");
+
+        assertThat(addIdConsumer.apply("1")).isEqualTo(3);
+        assertThat(mostFrequentSupplier.get()).containsExactly("1", "2");
+
+        assertThat(addIdConsumer.apply("2")).isEqualTo(2);
+        assertThat(mostFrequentSupplier.get()).containsExactly("1", "2");
+
+        assertThat(addIdConsumer.apply("3")).isEqualTo(1);
+        assertThat(mostFrequentSupplier.get()).containsExactly("1", "2", "3");
+
+        assertThat(addIdConsumer.apply("4")).isEqualTo(1);
+        assertThat(mostFrequentSupplier.get()).containsExactly("1", "2", "3");
+
+        assertThat(addIdConsumer.apply("3")).isEqualTo(2);
+        assertThat(mostFrequentSupplier.get()).containsExactly("1", "2", "3");
+
+        assertThat(addIdConsumer.apply("3")).isEqualTo(3);
+        assertThat(mostFrequentSupplier.get()).containsExactly("1", "3", "2");
+
+        assertThat(addIdConsumer.apply("1")).isEqualTo(4);
+        assertThat(mostFrequentSupplier.get()).containsExactly("1", "3", "2");
+
+        assertThat(addIdConsumer.apply("3")).isEqualTo(4);
+        assertThat(mostFrequentSupplier.get()).containsExactly("1", "3", "2");
+
+        assertThat(addIdConsumer.apply("3")).isEqualTo(5);
+        assertThat(mostFrequentSupplier.get()).containsExactly("3", "1", "2");
+    }
+}
