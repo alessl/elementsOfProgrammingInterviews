e1dc750e4dccd23cdb73bd69e69ff26fa9a627cb
Oleksandr Leonenko <aleks.leonenko@gmail.com>
2019-05-02 21:27:44 -0700

    7.2. Reverse sub list of a singly linked list

---
+++ Java/src/main/com/leonenko/epi/_7_list_TODO/_72_ReverseSubList.java
@@ -0,0 +1,30 @@
+package com.leonenko.epi._7_list_TODO;
+
+import java.util.stream.IntStream;
+
+
+/**
+ * Created by Alex on 5/12/2017.
+ */
+public class _72_ReverseSubList {
+
+    public static <T extends Comparable<T>> ListNode<T> reverseSubList(
+            ListNode<T> listHead, int from, int to) {
+        var newListHead = new ListNode<T>(null, listHead, null);
+        var subListHead = newListHead;
+        for (int i = 0; i < from; i++) {
+            subListHead = subListHead.getNext();
+        }
+
+        var iterator = subListHead.getNext();
+        for (int i = from; i < to; i++) {
+            var temp = iterator.getNext();
+            iterator.setNext(temp.getNext());
+            temp.setNext(subListHead.getNext());
+            subListHead.setNext(temp);
+        }
+
+        return newListHead.getNext();
+    }
+
+}
---
+++ Java/src/tests/com/leonenko/epi/_7_list_TODO/_72_ReverseSubListTest.java
@@ -0,0 +1,38 @@
+package com.leonenko.epi._7_list_TODO;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _72_ReverseSubListTest {
+
+    @Test
+    void reverseSubList() {
+        var listHead = ListNode.generateList(List.of(1, 2, 6, 5, 4, 3, 7));
+        var reversedListHead = _72_ReverseSubList.reverseSubList(listHead, 2, 5);
+
+        assertList(reversedListHead, List.of(1, 2, 3, 4, 5, 6, 7));
+    }
+
+    @Test
+    void reverseListAll() {
+        var listHead = ListNode.generateList(List.of(5, 4, 3, 2, 1));
+        var reversedListHead = _72_ReverseSubList.reverseSubList(listHead, 0, 4);
+
+        assertList(reversedListHead, List.of(1, 2, 3, 4, 5));
+    }
+
+    @Test
+    void reverseSubListMidToEnd() {
+        var listHead = ListNode.generateList(List.of(1, 2, 3, 6, 5, 4));
+        var reversedListHead = _72_ReverseSubList.reverseSubList(listHead, 3, 5);
+
+        assertList(reversedListHead, List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    private static void assertList(Iterable<Integer> list, List<Integer> keys) {
+        assertThat(list).containsExactlyElementsOf(keys);
+    }
+}
